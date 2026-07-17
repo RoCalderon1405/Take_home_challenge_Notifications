df0ddf7472ef169fbcdd7c3492491cb6488fadf1
RoCalderon <rocalderon1405@hotmail.com>
2026-07-16 21:27:49 -0600

    feat: set up Docker environment with PostgreSQL and backend service; add configuration files and update app module

--- backend_notifications/src/config/config.ts
+++ backend_notifications/src/config/config.ts
@@ -4,10 +4,10 @@ import { z } from 'zod';
 export const envSchema = z
   .object({
     PORT: z
-      .number()
-      .int('PORT must be an integer')
-      .positive('PORT must be a positive'),
-    allowedOrigins: z
+      .string()
+      .min(1, { message: 'PORT is required' })
+      .transform(Number),
+    ALLOWED_ORIGINS: z
       .string()
       .min(1, { message: 'allowedOrigins is required' })
       .transform((val) => val.split(',').map((origin) => origin.trim())),
@@ -18,6 +18,9 @@ type EnvType = z.infer<typeof envSchema>;
 
 const envParsed = envSchema.safeParse(process.env);
 
+console.log(process.env.ALLOWED_ORIGINS);
+
+
 if (!envParsed.success) {
   console.log(' config validations error:', z.treeifyError(envParsed.error));
   throw new Error('Invalid environment variables');
@@ -25,7 +28,7 @@ if (!envParsed.success) {
 
 export const Envs: EnvType = {
   PORT: envParsed.data.PORT,
-  allowedOrigins: envParsed.data.allowedOrigins,
+  ALLOWED_ORIGINS: envParsed.data.ALLOWED_ORIGINS,
   DATABASE_URL: process.env.DATABASE_URL || '',
   REDIS_URL: process.env.REDIS_URL || '',
   JWT_SECRET: process.env.JWT_SECRET || '',
